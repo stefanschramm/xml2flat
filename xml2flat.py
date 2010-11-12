69a1aa24154b7ea3a7e5b74e2fccafc2dc8a1fde
Stefan Schramm <mail@stefanschramm.net>
2010-11-12 01:37:21 +0100

    it's working :D

--- xml2flat.py
+++ xml2flat.py
@@ -18,44 +18,97 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# examples:
+# wget http://events.ccc.de/congress/2010/Fahrplan/schedule.en.xml -q -O - | ./xml2flat.py "//event" "../../@date" "start" "room" "title" -d " "
+# wget http://www.heise.de/newsticker/heise-atom.xml -q -O - | ./xml2flat.py -n "atom" "http://www.w3.org/2005/Atom" "//atom:entry" "atom:link/@href" "atom:title"
+# wget http://www.heise.de/newsticker/heise.rdf -q -O - | ./xml2flat.py -n "rdf" "http://my.netscape.com/rdf/simple/0.9/" "//rdf:item" "rdf:link" "rdf:title"
+
 import libxml2
 import sys
 
-# TODO:
-# - input file or stdin
-# - register namespaces by cmdline argument (or remove namespaces from document root node?)
-# - catch other exceptions (xpath syntax error, broken document, etc.)
-# - what, if a know wasn't found (xpath returned empty list) - empty string? - exception?
+def parse_cmdline_arguments(args):
 
-delimiter = "\t"
+    # defaults
+    filename = None
+    delimiter = "\t"
+    loop_expression = None
+    column_expressions = []
+    namespaces = []
+    stop_if_node_not_found = False
 
-#RSS:
-#loop_expression = "//rss:item"
-#namespaces = [["rss", "http://purl.org/rss/1.0/"]]
-#column_expressions = ["rss:link", "rss:title"]
-
-#ATOM:
-namespaces = [["atom", "http://www.w3.org/2005/Atom"]]
-loop_expression = "//atom:entry"
-column_expressions = ["atom:link/@href", "atom:title"]
-#example data: http://www.heise.de/newsticker/heise-atom.xml
-
-doc = libxml2.parseFile("test.xml")
-context = doc.xpathNewContext()
-for namespace in namespaces:
-    context.xpathRegisterNs(namespace[0], namespace[1])
-nodelist = context.xpathEval(loop_expression)
-for node in nodelist:
-    context.setContextNode(node)
-    fields = []
-    for column_expression in column_expressions:
-        column_nodelist = context.xpathEval(column_expression)
+    # parsing and syntax check
+    args = iter(args)
+    while True:
         try:
-            fields.append(column_nodelist[0].content)
-        except IndexError:
-            # node wasn't found - place empty string # TODO: make option
-            fields.append("")
-    print delimiter.join(fields)
-doc.freeDoc()
-context.xpathFreeContext()
+            arg = args.next()
+        except StopIteration:
+            break
+        if arg == '-f':
+            # filename
+            try:
+                filename = args.next()
+                continue
+            except StopIteration:
+                sys.exit("Missing argument for -f (filename)")
+        if arg == '-d':
+            # delimiter
+            try:
+                delimiter = args.next()
+                continue
+            except StopIteration:
+                sys.exit("Missing argument for -d (field delimiter)")
+        if arg == '-n':
+            # namespace (prefix and url)
+            try:
+                namespaces.append([args.next(), args.next()])
+                continue
+            except StopIteration:
+                sys.exit("Missing argument for -n (namespace prefix) (namespace url)")
+        if arg == '-s':
+            # stop if column node not found
+            stop_if_node_not_found = True
+            continue
+        if loop_expression == None:
+            loop_expression = arg
+            continue
+        else:
+            column_expressions.append(arg)
+            continue
+        sys.exit("Unknown option: " + arg)
 
+    return filename, delimiter, loop_expression, column_expressions, namespaces, stop_if_node_not_found
+
+if __name__ == "__main__":
+
+    try:
+        # parse command line options
+        (filename, delimiter, loop_expression, column_expressions, namespaces, stop_if_node_not_found) = parse_cmdline_arguments(sys.argv[1:])
+
+        # determine data source (file or stdin)
+        if filename != None and filename != "-":
+            doc = libxml2.parseFile(filename)
+        else:
+            doc = libxml2.parseDoc(sys.stdin.read())
+        
+        context = doc.xpathNewContext()
+        for namespace in namespaces:
+            context.xpathRegisterNs(namespace[0], namespace[1])
+        nodelist = context.xpathEval(loop_expression)
+        for node in nodelist:
+            context.setContextNode(node)
+            fields = []
+            for column_expression in column_expressions:
+                column_nodelist = context.xpathEval(column_expression)
+                try:
+                    fields.append(column_nodelist[0].content)
+                except IndexError:
+                    if stop_if_node_not_found:
+                        sys.exit("Column node not found. XPath expression was: \"" + column_expression + "\". Remove -s option to get an empty string for this field.")
+                    else:
+                        fields.append("")
+            print delimiter.join(fields)
+        doc.freeDoc()
+        context.xpathFreeContext()
+
+    except KeyboardInterrupt:
+        sys.exit("Aborted by user")
